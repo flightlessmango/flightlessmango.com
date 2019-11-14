834179b65bdaab5a7ad3e00a5ebe787cab49d602
FlightlessMango <flightlessmangoyt@gmail.com>
2019-11-14 05:06:59 +0100

    Permit non-logged in user to view user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   
   def show
     @user = User.find(params[:id])
