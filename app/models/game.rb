db01175cdea413c5e9be6d666014febab466bdf3
FlightlessMango <flightlessmangoyt@gmail.com>
2019-12-12 02:55:45 +0100

    Fixed stupidly expensive query

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   has_and_belongs_to_many :benches
-  has_many :inputs
-  has_many :types, -> { distinct }, through: :inputs
+  has_many :inputs, -> { distinct }, through: :benches
+  has_many :types, -> { distinct }, through: :benches
   has_many :logs
 end
