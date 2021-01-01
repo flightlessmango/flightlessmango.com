339bb110707443ae634607117dee2173c6ce17bf
FlightlessMango <flightlessmangoyt@gmail.com>
2021-01-01 01:46:22 +0100

    Save profile image to s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   has_many :computers
   devise :database_authenticatable, #:registerable,
          :rememberable, :omniauthable, omniauth_providers: %i[discord]
- 
+  has_one_attached :profile
+  
   def self.from_omniauth(auth)
    self.update_omniauth_user(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+   @user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      password = Devise.friendly_token[0, 20]
      user.email = auth.uid
      user.password = password
@@ -14,10 +15,14 @@ class User < ApplicationRecord
      user.username = auth.info.name   # assuming the user model has a name
      user.image = auth.info.image # assuming the user model has an image
      user.admin = false
+     
      # If you are using confirmable and the provider(s) you use validate emails, 
      # uncomment the line below to skip the confirmation emails.
      #user.skip_confirmation!
    end
+   require 'open-uri'
+   @user.profile.attach(io: open(auth.info.image), filename: "profile.png")
+   return @user
   end
   
   def self.update_omniauth_user(auth)
