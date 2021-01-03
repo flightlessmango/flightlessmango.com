64fdd8c92b0037077771044a9fa0f6a61c2ab644
flightlessmango <flightlessmangoyt@gmail.com>
2021-01-03 01:43:01 +0100

    Show unpublished benches to Mango

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
     def index
-        @benchmarks = Bench.all.where(published: true)
+        if user_signed_in? && current_user.admin?
+            @benchmarks = Bench.all
+        else
+            @benchmarks = Bench.all.where(published: true)
+        end
         @user_benchmarks = Log.all
     end
 end
